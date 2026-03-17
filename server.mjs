736403d110e5ec79f0149f7bfa6bbca396662855
khk4912 <kosame@kosame.dev>
2026-03-17 23:50:23 +0900

    ✨ feat: Setup bare express project

--- server.mjs
+++ server.mjs
@@ -2,21 +2,10 @@
 
 import express from 'express'
 
-import EventEmitter from 'events'
-import { logEvents } from './logEvents.js'
-
-class Emitter extends EventEmitter {}
-
-const PORT = process.env.PORT || 3500
-
-const myEmitter = new Emitter()
-myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName))
-
-console.log('Name: Ha Gyun Kim')
-
 const app = express()
+const PORT = process.env.PORT || 3500
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
+  console.log('Name: Ha Gyun Kim')
 })
-app.addListener('log', (msg, fileName) => logEvents(msg, fileName))
